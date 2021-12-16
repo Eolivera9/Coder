dd7487b9b818dce1bf013759601c5a1b25d08153
Ezequiel <ezequielcantarero@gmail.com>
2021-12-15 23:42:29 -0300

    complete entrega intermedia tp

--- ProyectoAplicacionCoder/ProyectoAplicacionCoder/AppCoder/urls.py
+++ ProyectoAplicacionCoder/ProyectoAplicacionCoder/AppCoder/urls.py
@@ -6,7 +6,10 @@ urlpatterns = [
     path('inicio', views.inicio, name="Inicio"),
     path('jugadores', views.jugadores, name="Jugadores"),
     path('equipos', views.equipos, name="Equipos"),
+    path('estadio', views.estadio, name="Estadio"),
     path('estadioFormulario', views.estadioFormulario),
+    path('equipoFormulario', views.equipoFormulario),
+    path('jugadorFormulario', views.jugadorFormulario),
     path('busquedaEquipo', views.busquedaEquipo),
     path('buscar/', views.buscar),
 
--- ProyectoAplicacionCoder/ProyectoAplicacionCoder/AppCoder/views.py
+++ ProyectoAplicacionCoder/ProyectoAplicacionCoder/AppCoder/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from AppCoder.models import Estadio, Equipo
-from AppCoder.forms import EstadioFormulario
+from AppCoder.models import Estadio, Equipo, Jugador
+from AppCoder.forms import EstadioFormulario, EquipoFormulario, JugadorFormulario
 # Create your views here.
 
 def inicio(request):
@@ -11,6 +11,10 @@ def inicio(request):
 def jugadores(request):
 
     return render(request, 'AppCoder/jugadores.html')
+
+def estadio(request):
+
+    return render(request, 'AppCoder/estadio.html')
 
 def estadioFormulario(request):
 
@@ -34,6 +38,50 @@ def estadioFormulario(request):
 
     return render(request, 'AppCoder/estadioFormulario.html',{"miFormulario":miFormulario})
 
+def jugadorFormulario(request):
+
+    if request.method == "POST":
+
+        miFormulario = JugadorFormulario(request.POST)
+
+        if miFormulario.is_valid():
+
+            informacion = miFormulario.cleaned_data
+
+            jugador = Jugador(apellido=informacion["apellido"] ,numero= informacion["numero"], esBueno=informacion["esBueno"])
+
+            jugador.save()
+
+            return render(request, 'AppCoder/inicio.html')
+
+    else:
+
+        miFormulario = JugadorFormulario()
+
+    return render(request, 'AppCoder/jugadorFormulario.html',{"miFormulario":miFormulario})
+
+def equipoFormulario(request):
+
+    if request.method == "POST":
+
+        miFormulario = EquipoFormulario(request.POST)
+
+        if miFormulario.is_valid():
+
+            informacion = miFormulario.cleaned_data
+
+            equipo = Equipo(nombre=informacion["nombre"] ,ciudad= informacion["ciudad"])
+
+            equipo.save()
+
+            return render(request, 'AppCoder/inicio.html')
+
+    else:
+
+        miFormulario = EquipoFormulario()
+
+    return render(request, 'AppCoder/equipoFormulario.html',{"miFormulario":miFormulario})
+
 def equipos(request):
 
     return render(request, 'AppCoder/equipos.html')
@@ -52,7 +100,7 @@ def buscar(request):
 
         #respuesta = f"Estoy buscando a: {request.GET['nombre']}"
 
-        return render(request, "AppCoder/resultadoBusqueda.html", {"nombre":nombre, "ciudad":ciudad })
+        return render(request, "AppCoder/resultadoBusqueda.html", {"equipos":equipos, "nombre":nombre })
 
 
 
